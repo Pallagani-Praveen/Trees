131fad88df839269a88b07e58b24c9742d6c28b3
Pallagani-Praveen <praveenpallagani999@gmail.com>
2021-07-29 10:55:43 +0530

    pre-order and post-order and depths and balanced

--- BST/CreateBST.java
+++ BST/CreateBST.java
@@ -1,15 +1,24 @@
 package BST;
 
+import java.util.Arrays;
+
 
 class Node {
     int value;
     Node  left;
     Node right;
     Node() {}
+
     Node(int value) { 
         this.value = value;
         this.left = null;
         this.right = null;
+    }
+
+    Node (int value,Node left,Node right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
     }
 }
 
@@ -46,11 +55,51 @@ public class CreateBST {
         this.head = head;
     }
 
+    public Node balanceTree(int[] nodes,int start,int end) {
+        if(start>=end) {
+            return null;
+        } else {
+            int mid = (start+end)/2;
+            return new Node(nodes[mid],this.balanceTree(nodes,start,mid),this.balanceTree(nodes,mid+1,end));
+        }
+    }
+
+    public void createBalancedBSTree(int[] nodes) {
+        int n = nodes.length;
+        Arrays.sort(nodes);
+        Node head = this.balanceTree(nodes,0,n);
+        this.head = head;
+    }
+
     public void InorderTraversal(Node node) {
         if(node!=null) {
             this.InorderTraversal(node.left);
             System.out.print(node.value + " ");
             this.InorderTraversal(node.right);
+        }
+    }
+
+    public void PreorderTraversal(Node node) {
+        if(node!=null) {
+            System.out.print(node.value + " ");
+            this.InorderTraversal(node.left);
+            this.InorderTraversal(node.right);
+        }
+    }
+
+    public void PostorderTraversal(Node node) {
+        if(node!=null) {
+            this.InorderTraversal(node.left);
+            this.InorderTraversal(node.right);
+            System.out.print(node.value + " ");
+        }
+    }
+
+    public void depths(Node node,int level) {
+        if(node!=null) {
+            this.depths(node.left, level+1);
+            System.out.print(node.value+"{"+level+"} ");
+            this.depths(node.right, level+1);
         }
     }
 }
--- Trees.java
+++ Trees.java
@@ -4,8 +4,22 @@ public class Trees {
     public static void main(String[] args) {
         CreateBST tree = new CreateBST();
         int[] nodes = {10,5,15,3,7,1,9,2,8};
-        tree.createNormalBSTree(nodes);
+        // tree.createNormalBSTree(nodes); // creates a normal bstree without balancing
+        tree.createBalancedBSTree(nodes);
+        System.out.print("PreOrder Traversal : ");
+        tree.PreorderTraversal(tree.head);
+        System.out.println("");
+
         System.out.print("InOrder Traversal : ");
         tree.InorderTraversal(tree.head);
+        System.out.println("");
+
+        System.out.print("PostOrder Traversal : ");
+        tree.PostorderTraversal(tree.head);
+        System.out.println("");
+
+        System.out.print("Nodes with depths : ");
+        tree.depths(tree.head,0);
+        System.out.println("");
     }
 }
